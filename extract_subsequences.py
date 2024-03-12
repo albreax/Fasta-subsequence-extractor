193f7c41cf57cdf7b0af9c14ae16077f01d60007
Albrecht Naumann <albrecht.naumann@posteo.de>
2024-03-12 20:55:39 +0100

    first commit

---
+++ extract_subsequences.py
@@ -0,0 +1,138 @@
+#!/usr/bin python
+import sys
+import os
+import re
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+
+__author__ = 'albrecht.naumann@posteo.de'
+
+"""
+The script extract subsequences from a given fasta file by a given
+coordinate txt. All extracted subsequences resulting in a output fasta file.
+"""
+
+
+def is_valid_file(s_file_path, s_file_type):
+    """return True if file exists"""
+    return (os.path.isfile(s_file_path) and
+            s_file_path.lower().endswith(s_file_type))
+
+
+def parse_coordinate_line(line):
+    """
+    parse a line of the coordinate file and return a coordinate dictionary
+    """
+
+    # return if line is empty or a comment
+    l = re.sub(re.compile(r'\s+'), '', line)
+    if l == '' or l.startswith("#"):
+        return
+
+    pttn = re.compile(r"(.+)\s+([0-9]+)\s+([0-9]+)")
+    m = re.match(pttn, line)
+
+    if not m:
+        raise Exception("ERROR coordinate file error in line:\n\t%s"
+                        % (line)
+                        )
+    # create coordinate dictionary
+    c = {
+        "id": m.group(1),
+        "start": int(m.group(2)),
+        "end": int(m.group(3))
+    }
+
+    if c["end"] < c["start"]:
+        start = c["end"]
+        c["end"] = c["start"]
+        c["start"] = start
+
+    return c
+
+
+def main(argv):
+    fasta_file = False
+    coordinate_file = False
+    output_file = False
+
+    args = ' '.join(argv)
+
+    for arg in ['-s', '-c', '-o']:
+        regex = re.compile(r"%s\s+(\S+)" % (arg))
+        m = re.search(regex, args)
+
+        if not m:
+            sys.exit(
+                "ERROR: Missing parameter\n" +
+                "read README.md for more information")
+
+        if arg == '-s' and is_valid_file(m.group(1), '.fasta'):
+            fasta_file = m.group(1)
+        elif arg == '-c' and is_valid_file(m.group(1), '.txt'):
+            coordinate_file = m.group(1)
+        elif arg == '-o':
+            output_file = m.group(1)
+
+    # is a fasta file passed
+    if not fasta_file:
+        sys.exit(
+            "ERROR: Missing fasta file\nread README.md for more information")
+    if not coordinate_file:
+        sys.exit(
+            "ERROR: Missing coordinate " +
+            "file\nread README.md for more information")
+    if not output_file:
+        sys.exit(
+            "ERROR: Missing output fasta " +
+            "file\nread README.md for more information")
+
+    coordinates = {}
+    # read given coordinate file and save each coordinate
+    with open(coordinate_file) as fh:
+        for line in fh:
+            # save sequence ID start and end in coordinates
+            try:
+                result = parse_coordinate_line(line)
+                if not result:
+                    continue
+
+                if not result['id'] in coordinates:
+                    coordinates[result['id']] = []
+
+                c = {"start": result['start'], "end": result['end']}
+
+                if c not in coordinates[result['id']]:
+                    coordinates[result['id']].append(c)
+
+            except Exception as e:
+                sys.exit(e)
+    # clear file
+    open(output_file, 'w').close()
+    oh = open(output_file, "a")
+
+    # read given fasta file and process each sequence record
+    for seq_record in SeqIO.parse(fasta_file, "fasta"):
+
+        if seq_record.id in coordinates:
+            for c in coordinates[seq_record.id]:
+                # extract fragment sequence
+                fragment = Seq(str(seq_record.seq)[c["start"] - 1:c["end"]])
+                # fragment description
+                description = "fragment start: %s end: %s" % (
+                    str(c["start"]), str(c["end"]))
+                # create output record
+                out_record = SeqRecord(
+                    fragment.upper(),
+                    id=seq_record.id,
+                    description="<%s>" % (description)
+                )
+                # append output record to output sequences
+                SeqIO.write(out_record, oh, "fasta")
+
+    oh.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
+    print("done")
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Fasta Subsequence Extractor
+This Python script extracts subsequences from a given FASTA file based on coordinates specified in a text file. The extracted subsequences are saved to an output FASTA file. It leverages the BioPython library to handle FASTA file parsing and subsequence extraction. This README outlines the script's functionality, prerequisites, and usage instructions.
+
+# Prerequisites
+Python 3.x
+BioPython library
+Before running the script, ensure you have Python 3.x installed on your system and the BioPython library. You can install BioPython using pip:
+
+```sh
+pip install biopython
+```
+
+# Script Overview
+The script takes three main arguments:
+
+A FASTA file (-s) containing the sequences from which subsequences will be extracted.
+A coordinate file (-c) in text format specifying the ID, start, and end positions for each subsequence to be extracted.
+An output FASTA file (-o) where the extracted subsequences will be saved.
+The coordinate file should be formatted with one entry per line, where each line contains the sequence ID followed by the start and end positions of the subsequence, separated by whitespace. Lines starting with "#" are considered comments and ignored.
+
+# Usage
+Run the script from the command line by providing the required arguments:
+
+```sh
+./your_script_name.py -s your_fasta_file.fasta -c your_coordinate_file.txt -o output_file.fasta
+```
+
+Replace your_script_name.py with the name of this Python script, your_fasta_file.fasta with the path to your input FASTA file, your_coordinate_file.txt with the path to your coordinate file, and output_file.fasta with the desired path for the output FASTA file containing the extracted subsequences.
+
+# Example Coordinate File
+
+```sql
+#SequenceID Start End
+Seq1 100 200
+Seq2 150 250
+```
+
+This example specifies that a subsequence from Seq1 starting at position 100 and ending at position 200 should be extracted, and another subsequence from Seq2 starting at position 150 and ending at position 250 should be extracted.
+
+# License
+This script is provided "as is", without warranty of any kind. You are free to use, modify, and distribute it as you see fit.
